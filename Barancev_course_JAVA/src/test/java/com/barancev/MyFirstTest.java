60d755067a187c6665b8b4d4b68b4edb63e22500
eyatsenko <eduardyacenko@gmail.com>
2020-12-25 14:14:46 +0200

    Something changed

--- Barancev_course_JAVA/src/test/java/com/barancev/MyFirstTest.java
+++ Barancev_course_JAVA/src/test/java/com/barancev/MyFirstTest.java
@@ -6,8 +6,10 @@ import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import static org.openqa.selenium.support.ui.ExpectedConditions.titleIs;
@@ -26,14 +28,29 @@ public class MyFirstTest {
 
     @Test
     public void myFirstTest(){
-        driver.navigate().to("http://www.google.com/");
-        driver.findElement(By.name("q")).sendKeys("webdriver", Keys.ENTER);
-        wait.until(titleIs("webdriver - Поиск в Google"));
+        login("admin", "admin");
+        String title = driver.getTitle();
+        wait.until(titleIs(title));
     }
 
     @After
     public void stop() {
         driver.quit();
         driver = null;
+    }
+
+    public void login (String login, String password){
+        driver.navigate().to("http://localhost/litecart/admin");
+        WebElement element = wait.until(
+                ExpectedConditions.visibilityOfElementLocated(By.name("username")));
+        driver.findElement(By.name("username")).click();
+        driver.findElement(By.name("username")).sendKeys(login);
+        WebElement element2 = wait.until(
+                ExpectedConditions.visibilityOfElementLocated(By.name("password")));
+        driver.findElement(By.name("password")).click();
+        driver.findElement(By.name("password")).sendKeys(password);
+        WebElement element3 = wait.until(
+                ExpectedConditions.visibilityOfElementLocated(By.name("login")));
+        driver.findElement(By.name("login")).click();
     }
 }
